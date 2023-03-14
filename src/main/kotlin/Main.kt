4d0b6d893a2e04784cdddf751611cbe8d518011d
Irakli Dekanosidze <88934243+iraklideka94@users.noreply.github.com>
2023-03-14 15:59:13 +0400

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,3 +14,22 @@ fun singleNumber(nums: IntArray): Int {
     }
     return result
 }
+
+
+//2. გვაქვს 1,5,10,20 და 50 თეთრიანი მონეტები. დაწერეთ ფუნქცია, რომელსაც გადაეცემა თანხა
+//(თეთრებში) და აბრუნებს მონეტების მინიმალურ რაოდენობას, რომლითაც შეგვიძლია ეს თანხა
+//დავახურდაოთ.
+Int minSplit(Int amount);
+fun minSplit(amount: Int): Int {
+    val coins = intArrayOf(50, 20, 10, 5, 1)
+    var numCoins = 0
+    var remainingAmount = amount
+    for (i in coins.indices) {
+        numCoins += remainingAmount / coins[i]
+        remainingAmount %= coins[i]
+    }
+    return numCoins
+}
+
+
+
